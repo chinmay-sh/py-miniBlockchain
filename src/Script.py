ef96b6568b6136387c956ba41a5f71f1c3e76b24
Chinmay <chinmaysharma45@gmail.com>
2020-05-20 21:06:30 +0530

    initial commit

---
+++ src/Script.py
@@ -0,0 +1,67 @@
+from blockchain import Blockchain
+
+transaction1 = {
+  'amount': '30',
+  'sender': 'Alice',
+  'receiver': 'Bob'}
+transaction2 = { 
+  'amount': '200',
+  'sender': 'Bob',
+  'receiver': 'Alice'}
+transaction3 = { 
+  'amount': '300',
+  'sender': 'Alice',
+  'receiver': 'Timothy' }
+transaction4 = { 
+  'amount': '300',
+  'sender': 'Rodrigo',
+  'receiver': 'Thomas' }
+transaction5 = { 
+  'amount': '200',
+  'sender': 'Timothy',
+  'receiver': 'Thomas' }
+transaction6 = { 
+  'amount': '400',
+  'sender': 'Tiffany',
+  'receiver': 'Xavier' }
+
+mempool = [transaction1, transaction2, transaction3, transaction4, transaction5, transaction6]
+
+my_transaction = {
+  'amount': '600',
+  'sender': 'Mouse',
+  'receiver': 'Rat' 
+}
+
+mempool.append(my_transaction)
+
+block_transactions = [transaction1,transaction3,my_transaction]
+
+
+
+new_transactions = [{'amount': '30', 'sender':'alice', 'receiver':'bob'},
+               	{'amount': '55', 'sender':'bob', 'receiver':'alice'}]
+
+my_blockchain = Blockchain()
+
+my_blockchain.add_block(new_transactions)
+
+print(my_blockchain.print_blocks())
+
+
+from blockchain import Blockchain
+
+block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
+block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
+block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
+fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"25"}
+
+local_blockchain = Blockchain()
+local_blockchain.print_blocks()
+
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
+local_blockchain.chain[2].transactions = fake_transactions
+local_blockchain.validate_chain()
